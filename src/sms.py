356bfdb90f9aec8bb75c34b31b8c8b4a2e06e6eb
Spacenin <chepstudios@gmail.com>
2022-04-12 15:50:42 -0400

    Added name and applicable weather locations, removed timer and used crontab instead

--- src/sms.py
+++ src/sms.py
@@ -4,7 +4,7 @@ import weather
 
 def sendMessage():
     #Get JSON details for various information to access Twilio
-    with open("../data/twilioapi.json") as jsonFile:
+    with open("/home/pi/Code/pys/weatherSend/data/twilioapi.json") as jsonFile:
         SMSdata = json.load(jsonFile)
 
     #Setup connection
@@ -13,19 +13,23 @@ def sendMessage():
     #Send text message to every recipient
     for number in SMSdata["ToPhones"]:
         #Get dictionary of JSON data from Weather API
-        dailyForecast = weather.getForecast()
-
+        dailyForecast = weather.getForecast(number[2])
+        
+        #Create message to send
         client.messages.create(
             body=composeMsg(number, dailyForecast),
             from_=SMSdata["FromPhone"],
-            to=str(number)
+            to=str(number[0])
         )
     
 def composeMsg(number, dailyForecast):
     message = \
-        "Good morning " + str(number) + "!\n" + \
+        "Good morning " + str(number[1]) + "!\n" + \
         "Max temp: " + str(dailyForecast["maxtemp_f"]) + "\n" + \
         "Min temp: " + str(dailyForecast["mintemp_f"]) + "\n" + \
         "Avg temp: " + str(dailyForecast["avgtemp_f"])
     
     return(message)
+    
+if __name__ == "__main__":
+    sendMessage()
--- src/weather.py
+++ src/weather.py
@@ -1,13 +1,13 @@
 import requests
 import json
 
-def getForecast():
+def getForecast(zipcode):
     #Get API key from weatherAPI JSON file
-    with open("../data/weatherapi.json") as jsonFile:
+    with open("/home/pi/Code/pys/weatherSend/data/weatherapi.json") as jsonFile:
         apiKey = json.load(jsonFile)
 
     #Get JSON of forecast data
-    forecastData = requests.get("http://api.weatherapi.com/v1/forecast.json?key="+apiKey["key"]+"&q=29631&days=1").json()
+    forecastData = requests.get("http://api.weatherapi.com/v1/forecast.json?key="+apiKey["key"]+"&q="+str(zipcode)+"&days=1").json()
 
     #Parse out the day data and return it to sms
     dailyForecast = forecastData["forecast"]["forecastday"][0]["day"]
