f4830f3000dbc22169ff9421361bdb43dd2cc049
Spacenin <chepstudios@gmail.com>
2022-04-12 16:12:25 -0400

    Fleshed out messages sent

--- src/sms.py
+++ src/sms.py
@@ -23,13 +23,18 @@ def sendMessage():
         )
     
 def composeMsg(number, dailyForecast):
+    #Create message with all applicable stats
     message = \
         "Good morning " + str(number[1]) + "!\n" + \
         "Max temp: " + str(dailyForecast["maxtemp_f"]) + "\n" + \
         "Min temp: " + str(dailyForecast["mintemp_f"]) + "\n" + \
-        "Avg temp: " + str(dailyForecast["avgtemp_f"])
-    
+        "Avg temp: " + str(dailyForecast["avgtemp_f"]) + "\n" + \
+        "Chance of rain: " + str(dailyForecast["daily_chance_of_rain"]) + "\n" + \
+        "Total rainfall: " + str(dailyForecast["totalprecip_in"]) + "\n" + \
+        "Max wind speed: " + str(dailyForecast["maxwind_mph"])
+        
     return(message)
-    
+
+#Ensure it runs the send message function
 if __name__ == "__main__":
     sendMessage()
